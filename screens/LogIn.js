2f88ec427a520c1f9541474806d72a98e8d9158c
SofranSebastian <sofransebastian@yahoo.com>
2021-11-09 18:48:52 +0200

    created the LogIn screen

---
+++ screens/LogIn.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Text, View, ImageBackground, } from 'react-native';
+import { TextInput, Button } from 'react-native-paper';
+
+
+export default class SignUp extends React.Component{
+
+    constructor(){
+        super();
+
+        this.state = {
+            isPasswordVisible: true,
+            iconNamePassword: "eye",
+        }
+    }
+
+    render(){
+        return(
+            <View>
+                <Text>asa</Text>
+            </View>
+        )
+    }
+
+}
