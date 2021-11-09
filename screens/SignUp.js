cad872d6186b75f92a9b35bbb50aaee485717834
SofranSebastian <sofransebastian@yahoo.com>
2021-11-09 18:37:36 +0200

    added the SignUp button

--- screens/SignUp.js
+++ screens/SignUp.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Text, View, ImageBackground, } from 'react-native';
-import { TextInput } from 'react-native-paper';
+import { TextInput, Button } from 'react-native-paper';
 
 const background_image_source = require('../images/signup-background.png');
 
@@ -8,6 +8,24 @@ export default class SignUp extends React.Component{
 
     constructor(){
         super();
+
+        this.state = {
+            isPasswordVisible: true,
+            iconNamePassword: "eye",
+        }
+    }
+
+    _onPasswordIconPress = () => {
+        this.setState({ isPasswordVisible: !this.state.isPasswordVisible });
+        if( this.state.isPasswordVisible === true ){
+            this.setState({ iconNamePassword : "eye" })
+        }else{
+            this.setState({ iconNamePassword : "eye-off" })
+        }
+    }
+
+    _onSignUpPress = () => {
+        console.log("Am apasat pe sign up");
     }
 
     render(){
@@ -19,34 +37,52 @@ export default class SignUp extends React.Component{
                                         flex:1,
                                     }} 
                 >
-                    <View style={{flex:0.40}}></View>
-                    <View style={{flex:0.40, justifyContent:'center', alignItems: 'center'}}>
+                    <View style={{flex:0.35}}></View>
+                    <View style={{flex:0.55, justifyContent:'center', alignItems: 'center'}}>
                         <TextInput
+                            theme={{ roundness: 20 }} 
                             style={{backgroundColor:"white", width:"90%"}}
                             outlineColor="#262731"
                             activeOutlineColor="#262731"
                             mode="outlined"
                             label="Username"
-                            placeholder="Type something"
+                            placeholder="Please type your username"
+                            right={<TextInput.Icon name="account-check" color="#262731" />}
                         />
                         <TextInput
-                            style={{backgroundColor:"white", width:"90%"}}
+                            theme={{ roundness: 20 }} 
+                            style={{backgroundColor:"white", width:"90%", marginTop:"5%"}}
                             outlineColor="#262731"
                             activeOutlineColor="#262731"
                             mode="outlined"
                             label="Email"
-                            placeholder="Type something"
+                            placeholder="Please type your email"
+                            right={<TextInput.Icon name="email" color="#262731" />}
                         />
                         <TextInput
-                            style={{backgroundColor:"white", width:"90%"}}
+                            secureTextEntry={ this.state.isPasswordVisible }
+                            theme={{ roundness: 20 }} 
+                            style={{backgroundColor:"white", width:"90%", marginTop:"5%"}}
                             outlineColor="#262731"
                             activeOutlineColor="#262731"
                             mode="outlined"
                             label="Password"
-                            placeholder="Type something"
+                            placeholder="Please type your password"
+                            right={ <TextInput.Icon     name={ this.state.iconNamePassword } 
+                                                        color="#262731" 
+                                                        onPress = { this._onPasswordIconPress }
+                                    />
+                            }
                         />
+                        <Button style={{backgroundColor:"#262731", marginTop:"10%", width:"40%", height: 40}}
+                                theme={{ roundness: 20 }}
+                                mode="contained"
+                                onPress = { this._onSignUpPress }
+                        >
+                            SIGN UP
+                        </Button>
                     </View>
-                    <View style={{flex:0.20}}></View>
+                    <View style={{flex:0.10}}></View>
 
                 </ImageBackground>
             </View>
