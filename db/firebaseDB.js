ac819015424d1f88e46442b4a08dbd2fb8020029
SofranSebastian <sofransebastian@yahoo.com>
2021-11-09 22:29:06 +0200

    created a separated config file for firebase connection

---
+++ db/firebaseDB.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection } from 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB5V08qRzexHyofw1aEqVnlhfuxHMqxty4",
+    authDomain: "teamdiff-stackly-24478.firebaseapp.com",
+    projectId: "teamdiff-stackly-24478",
+    storageBucket: "teamdiff-stackly-24478.appspot.com",
+    messagingSenderId: "631275236682",
+    appId: "1:631275236682:web:f54bdaa6a1f244510de5b3",
+    measurementId: "G-HEFWSMX049"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+export const usersCol = collection(db, "users");
