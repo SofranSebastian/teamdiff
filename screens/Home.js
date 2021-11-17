13c4157d5d83e6c34a8f3b662d1e92f4244d912c
SofranSebastian <sofransebastian@yahoo.com>
2021-11-17 22:32:07 +0200

    made the HTTP request with the GET method for the news

--- screens/Home.js
+++ screens/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text, View, ImageBackground, } from 'react-native';
+import { Text, View, ImageBackground, SafeAreaView, FlatList } from 'react-native';
 import { TextInput, Button, HelperText } from 'react-native-paper';
 import { usersCol } from "../db/firebaseDB";
 import { getFirestore, collection, getDocs, addDoc, doc, query, where, updateDoc, arrayUnion } from 'firebase/firestore';
@@ -9,11 +9,35 @@ export default class Home extends React.Component {
         super();
     }
 
+    componentDidMount(){
+        fetch(  "https://current-news.p.rapidapi.com/news/technology", {
+                "method": "GET",
+                "headers": {
+                    "x-rapidapi-host": "current-news.p.rapidapi.com",
+                    "x-rapidapi-key": "baf44d4290msh54dc398cb97add8p194c17jsn9aab1e258a32"
+                }
+            })
+            .then( 
+                (response) => response.json()
+            ).then( (responseData) =>
+                console.log(responseData)
+            )
+            .catch(err => {
+                console.error(err);
+            });
+    }
+
     render() {
         return (
-            <View>
+            <SafeAreaView style={{flex: 1}}>
+                    <FlatList
+                        data={DATA}
+                        renderItem={renderItem}
+                        keyExtractor={item => item.id}
+                    />
+                    
                 <Text style={ {fontSize: 20} }>Home</Text>
-            </View>
+            </SafeAreaView>
         )   
     }
 }
