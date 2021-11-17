759b539fc7f1877c4433fb584c2cd38cd2ea5184
SofranSebastian <sofransebastian@yahoo.com>
2021-11-18 01:19:03 +0200

    modified the props for the card component

--- components/CardNews.js
+++ components/CardNews.js
@@ -1,21 +1,33 @@
 import * as React from 'react';
+import { TouchableOpacity,  Linking, Text } from 'react-native';
 import { Avatar, Button, Card, Title, Paragraph } from 'react-native-paper';
 
-const LeftContent = props => <Avatar.Icon {...props} icon="folder" />
+function CardNews(props){
+    return(
+        <Card style={{  marginHorizontal:5, 
+                        width:250, 
+                        borderRadius:15
+                    }}
+        >
+            <Card.Title title={ props.title }
+                        titleStyle={{ fontSize:12, fontFamily:'normal-font', fontWeight:'bold', lineHeight:15 }}
+                        titleNumberOfLines={4}
+            />
+            <Card.Cover source={{ uri: props.urlToImage }} />
+            <Card.Actions style={{ justifyContent:'flex-end' }}>
+                <TouchableOpacity   style={{ flexDirection:'row', alignItems:'center' }}
+                                    onPress={ () => {
+                                    Linking.openURL(props.url)
+                                    .catch(err => {
+                                        console.error("Failed opening page because: ", err)
+                                        alert('Failed to open page')
+                                    })}}>
+                    <Avatar.Icon size={25} icon="launch" color="#262731" style={{backgroundColor:'white'}} />
+                    <Text style={{ fontSize:10, fontFamily:'normal-font', fontWeight:'bold' }}>SEE MORE</Text>
+                </TouchableOpacity>
+            </Card.Actions>
+        </Card>
+    )
+};
 
-const MyComponent = () => (
-  <Card>
-    <Card.Title title="Card Title" subtitle="Card Subtitle" left={LeftContent} />
-    <Card.Content>
-      <Title>Card title</Title>
-      <Paragraph>Card content</Paragraph>
-    </Card.Content>
-    <Card.Cover source={{ uri: 'https://picsum.photos/700' }} />
-    <Card.Actions>
-      <Button>Cancel</Button>
-      <Button>Ok</Button>
-    </Card.Actions>
-  </Card>
-);
-
-export default MyComponent;
+export default CardNews;
