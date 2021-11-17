357e227360bca89dcfec62ef868cbcc8f3be7432
SofranSebastian <sofransebastian@yahoo.com>
2021-11-18 01:19:42 +0200

    improved the FlatList for the Home screen

--- screens/Home.js
+++ screens/Home.js
@@ -1,16 +1,31 @@
 import React from "react";
-import { Text, View, ImageBackground, SafeAreaView, FlatList } from 'react-native';
+import { Text, View, ImageBackground, SafeAreaView, FlatList, ScrollView, StatusBar } from 'react-native';
 import { TextInput, Button, HelperText } from 'react-native-paper';
 import { usersCol } from "../db/firebaseDB";
 import { getFirestore, collection, getDocs, addDoc, doc, query, where, updateDoc, arrayUnion } from 'firebase/firestore';
-import { CardNews } from "../components/CardNews";
-
+import CardNews from "../components/CardNews";
+const DATA = [
+    {
+      id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
+      title: "First Item",
+    },
+    {
+      id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
+      title: "Second Item",
+    },
+    {
+      id: "58694a0f-3da1-471f-bd96-145571e29d72",
+      title: "Third Item",
+    },
+  ];
 const dummyData = require('../dummyDataNews.json')
 export default class Home extends React.Component {
     constructor() {
         super();
     
-        this.data = []
+        this.data = dummyData.news
+        console.log(this.data);
+
     }
 
     componentDidMount(){
@@ -31,20 +46,35 @@ export default class Home extends React.Component {
         //     .catch(err => {
         //         console.error(err);
         //     });
+        //this.data = dummyData.news;
+
+        // for( var i in dummyData.news.length){
+        //     this.data.push( dummyData.news[i] );
+        // }
+        // console.log(this.data);
+    
+        this.data = dummyData.news
     }
+
 
     render() {
         return (
-            <SafeAreaView style={{flex: 1}}>
-                    <FlatList
-                        data={ dummyData }
-                        horizontal = { true }
-                        renderItem={ <CardNews /> }
-                        keyExtractor={item => item.publishedAt}
+                <SafeAreaView>
+                    <View style={{ marginLeft:5, marginTop:'25%'}}>
+                        <Text style={{ fontSize:20, fontFamily:'normal-font', fontWeight:'bold' }}>TECH NEWS</Text>
+                    </View>
+                    <FlatList   scrollEnabled={ true }
+                                horizontal={ true }
+                                showsHorizontalScrollIndicator={ false }
+                                data={ this.data }
+                                renderItem={ ({item}) => <CardNews  title={ item.title }
+                                                                    urlToImage={ item.urlToImage }
+                                                                    url={ item.url }
+                                                        /> 
+                                        }
+                                keyExtractor={ item => item.publishedAt}
                     />
-                    
-                <Text style={ {fontSize: 20} }>Home</Text>
-            </SafeAreaView>
+                </SafeAreaView> 
         )   
     }
 }
