fe616ffe8a03f5f650a248a365ccc894997038d7
SofranSebastian <sofransebastian@yahoo.com>
2021-12-26 14:00:46 +0200

    changed the params for the navigation

--- components/BottomTabNavigator.js
+++ components/BottomTabNavigator.js
@@ -28,7 +28,7 @@ export default class BottomNavigator extends React.Component {
                                 }}
                         icon="plus"
                         color="white"
-                        onPress={ () => this.props.navigation.navigate("AddBug",{image:null})}
+                        onPress={ () => this.props.navigation.navigate("AddBug",{image:{uri:null}})}
                     />
                 </View>
                 <View style={{  position: 'absolute', 
