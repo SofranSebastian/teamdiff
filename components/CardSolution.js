4b7c6c204da1c038b85739bb6fba7c7868b59e20
SofranSebastian <sofransebastian@yahoo.com>
2021-12-26 08:33:37 +0200

    showing an icon for the best response from the responsesThread and modifying it in the db

--- components/CardSolution.js
+++ components/CardSolution.js
@@ -3,7 +3,7 @@ import { TouchableOpacity,  Linking, Text, View, Image } from 'react-native';
 import { Avatar, Button, Card, Title, Paragraph, IconButton } from 'react-native-paper';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { db, usersCol } from "../db/firebaseDB";
-import { doc, getDoc } from "firebase/firestore";
+import { arrayUnion, doc, getDoc, arrayRemove } from "firebase/firestore";
 import { collection, query, where, getDocs, updateDoc, increment, } from "firebase/firestore";
 
 async function getSolutionOwnerID(props){
@@ -33,14 +33,34 @@ async function getBugOwnerID(props){
     return id;
 }
 
+async function getResponse(props){
+    const bugRef = doc(db, "bugs", props.bugID);
+    const bug = await getDoc(bugRef);
+    
+    for( let i = 0 ; i < bug.data().responsesThread.length ; i++ ){
+        if( bug.data().responsesThread[i].createdAt === props.timestamp){
+                let newResponse = bug.data().responsesThread[i]
+                newResponse.isBest = true;
+                await updateDoc( bugRef, {
+                    responsesThread: arrayRemove(bug.data().responsesThread[i])
+                })
+                await updateDoc( bugRef, {
+                    responsesThread: arrayUnion(newResponse)
+                })
+        }   
+    }
+}
+
 async function markResolved(props){
     let userSolutionID = await getSolutionOwnerID(props);
     let userBugID = await getBugOwnerID(props);
     let bugID = props.bugID
 
+
     console.log(userSolutionID)
     console.log(bugID)
-
+    console.log(props.timestamp)
+    await getResponse(props)
     const userRef = doc(db, "users", userSolutionID);
     await updateDoc(userRef, {
         bugsScore: increment(+Number(props.bugPoints)),
@@ -51,6 +71,18 @@ async function markResolved(props){
     await updateDoc(bugRef, {
         isResolved:true
     });  
+
+    props.navigation.reset({
+        index: 0,
+        routes: [{ name: "BugDetail", params:{ 
+                                                screenTitle: props.screenTitle,
+                                                bugDetail: props.bugDetail,
+                                                bugPoints: props.bugPoints,
+                                                id: props.bugID,
+                                                creatorName: props.bugCreator,
+                                            }
+        }]
+    }) 
 }
 
 function CardSolution(props){
@@ -77,6 +109,14 @@ function CardSolution(props){
                             borderRadius:15,
                         }}
                 >
+                    
+                    { props.isBest === true ?
+                        <View style={{position:'absolute', right:5, top:5}}>
+                            <Avatar.Icon size={15} icon="trophy" style={{backgroundColor:"#262731"}}/>
+                        </View>
+                        :
+                        null
+                    }
                     <View style={{  flex:0.8, marginTop:'2%'}}>
                         {  props.bugCreator === props.ownerUsername ?
                             <Text style={{ marginLeft:'4%', fontSize:16, fontFamily:'normal-font', lineHeight:15,  color:"#262731" }}>
@@ -112,7 +152,7 @@ function CardSolution(props){
                                         onPress={() => markResolved(props)}
                             />
                         :
-                            null
+                        null
                         }
                     </View>
                 </View>
