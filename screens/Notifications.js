8bc2d633cf8670b2d517bf893a9f463d389d3daa
SofranSebastian <sofransebastian@yahoo.com>
2022-01-04 18:49:43 +0200

    created a basic screen layout for the notifications screen

---
+++ screens/Notifications.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default class Notifications extends React.Component{
+    constructor(){
+        super()
+
+
+        this.state = {
+
+        }
+    }
+
+    async componentDidMount(){   
+         
+    }
+
+    render(){
+        return(
+            <View style={{flex:1, backgroundColor:'white'}}>
+                <Text>asd</Text>
+            </View>
+        )
+    }
+
+}
